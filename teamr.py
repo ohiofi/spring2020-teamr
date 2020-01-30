57a25ad0a75b1ad688765ab6a71fbca298a4009a
rishil77 <57910960+rishil77@users.noreply.github.com>
2020-01-30 11:54:58 -0500

    Update teamr.py

--- teamr.py
+++ teamr.py
@@ -1,1 +1,14 @@
+from random import *
+roomNumbers = [0,1,2,3,100,200,101,201,103,104,105,203,204,302,303.402,403]
+roomArray = [] 
+for i in range(999):
+    roomArray.append(False)
 
+
+
+roomArray[0] = "You find yourself in the basement of house on a chair. There is a loose nail in the chair. There are walls to the North and West of you. To the East there is a rug, and to the South there are stairs." 
+roomArray[1] = "To the South is the stairs. To the east there is something in a frame, but you don't know what it is."
+roomArray[100] = "You are standing on a very damp rug. There is a weird bump on the rug, it looks suspicious."
+roomArray[200] = "To the north and east there is a wall. To the south you see a something in a frame."
+roomArray[101] = "To the north of you there is a moist rug. To the east of you there is something that looks like a paper in a picture frame."
+roomArray[201] = "There is a wall to the east and south of you. In the corner you see a high school diploma in the picture frame."
